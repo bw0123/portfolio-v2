6fc598a6929fa311e0887e55249723625395ac00
Britney Wu <britney@macbook-air.myfiosgateway.com>
2022-03-22 01:04:51 -0400

    Testing push on my laptop

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import React from 'react'
 function App() {
   return (
     <div>
-     <h1 className="text-3xl font-bold underline">Hello</h1>
+     <h1 className="text-3xl font-bold underline">Testing push on my laptop</h1>
     </div>
   );
 }
