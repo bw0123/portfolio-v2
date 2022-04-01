cb35dfd1cf878acd33013e751a5743926f495be8
Britney <Wu.Britney@gmail.com>
2022-04-01 16:56:07 -0400

    rm About btn icons && SwitchSection replace w/ daisyUI navBar

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,6 +1,5 @@
 import React from 'react'
-import { Layout, Button } from 'antd'
-import { LinkedinFilled, GithubFilled } from '@ant-design/icons'
+import { Layout } from 'antd'
 
 const { Content } = Layout;
 
@@ -23,12 +22,8 @@ function About() {
                         Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Maecenas hendrerit metus sit amet dolor malesuada sollicitudin. Vivamus dictum eu mauris vitae molestie. Suspendisse ac justo ut justo porttitor tempor. Mauris non tellus ac ligula vestibulum egestas. Mauris consectetur eu diam at vestibulum. Donec ut laoreet velit. Nam accumsan, ante id iaculis finibus, lorem ipsum scelerisque risus, non tempor dui metus sed sapien. Aliquam tempor nisl sit amet feugiat ornare. Fusce eleifend nisi id egestas bibendum. Proin tristique aliquet metus. Pellentesque porttitor, libero et vulputate pellentesque, justo nisi facilisis massa, tincidunt sodales velit sapien vitae urna. Fusce id dictum erat, sed maximus turpis.
                     </p>
                         <div className='flex justify-center p-10'>
-                            <Button type='primary' shape='circle'>
-                                <LinkedinFilled/>
-                            </Button>
-                            <Button type='primary' shape='circle'>
-                                <GithubFilled />
-                            </Button>
+                            <button class="btn btn-primary">LinkedIn</button>
+                            <button class="btn btn-primary">Github</button>
                         </div>
                 </div>
             </Content>
--- src/components/SwitchSection.jsx
+++ src/components/SwitchSection.jsx
@@ -1,11 +1,12 @@
 import React, { useState } from 'react'
-import { Layout, Menu } from 'antd'
+
+/** import { Layout, Menu } from 'antd' **/
 import About from './About'
 import WorkExperience from './WorkExperience'
 import Projects from './Projects'
 import Skills from './Skills'
 
-const { Header } = Layout;
+/** const { Header } = Layout; **/
 
 function SwitchSection() {
     const [current, setCurrent] = useState(0);
@@ -14,7 +15,7 @@ function SwitchSection() {
         setCurrent(0);
     }
 
-    const workExperienceSelected = () => {
+    const experienceSelected = () => {
         setCurrent(1);
     }
 
@@ -33,13 +34,62 @@ function SwitchSection() {
 
     return(
         <div>
+            <div class="navbar mb-2 shadow-lg bg-neutral text-neutral-content rounded-box">
+                <div class="flex-none px-2 mx-2">
+                    <span class="text-lg font-bold">
+                        Sample Title
+                    </span>
+            </div> 
+        <div class="flex-1 px-2 mx-2">
+            <div class="items-stretch hidden lg:flex">
+                <a class="btn btn-ghost btn-sm rounded-btn" onClick={() => aboutSelected()}>
+                About
+                </a> 
+                <a class="btn btn-ghost btn-sm rounded-btn" onClick={() => experienceSelected()}>
+                        Experience
+                </a> 
+                <a class="btn btn-ghost btn-sm rounded-btn" onClick={() => projectsSelected()}>
+                        Projects
+                </a> 
+                <a class="btn btn-ghost btn-sm rounded-btn" onClick={() => skillsSelected()}>
+                        Skills
+                </a>
+        </div>
+    </div> 
+    <div class="flex-none">
+        <button class="btn btn-square btn-ghost">
+        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" class="inline-block w-6 h-6 stroke-current">              
+            <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4.318 6.318a4.5 4.5 0 000 6.364L12 20.364l7.682-7.682a4.5 4.5 0 00-6.364-6.364L12 7.636l-1.318-1.318a4.5 4.5 0 00-6.364 0z"></path>            
+        </svg>
+        </button>
+    </div> 
+        <div class="flex-none">
+            <button class="btn btn-square btn-ghost">
+                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" class="inline-block w-6 h-6 stroke-current">             
+                    <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z"></path>             
+                </svg>
+            </button>
+        </div>
+        </div>
+            {current === 0 && <About />}
+            {current === 1 && <WorkExperience />}
+            {current === 2 && <Projects />}
+            {current === 3 && <Skills />}
+
+        </div>
+    )
+}
+
+export default SwitchSection
+
+ {/*
             <Header style={headerStyles}>
                 <Layout>
                     <Menu theme='dark' mode='horizontal' defaultSelectedKeys={['1']}>
                         <Menu.Item key='1' onClick={() => aboutSelected()}>
                             About
                         </Menu.Item>
-                        <Menu.Item onClick={() => workExperienceSelected()}>
+                        <Menu.Item onClick={() => experienceSelected()}>
                             Work Experience
                         </Menu.Item>
                         <Menu.Item onClick={() => projectsSelected()}>
@@ -48,14 +98,5 @@ function SwitchSection() {
                         <Menu.Item onClick={() => skillsSelected()}>Skills</Menu.Item>
                     </Menu>
                 </Layout>
-            </Header>
-                {current === 0 && <About />}
-                {current === 1 && <WorkExperience />}
-                {current === 2 && <Projects />}
-                {current === 3 && <Skills />}
-
-        </div>
-    )
-}
-
-export default SwitchSection
+            </Header>      
+    */}
