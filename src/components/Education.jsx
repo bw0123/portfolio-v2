fcba4b1cf3bf1f10baf28d05205033afcf5e610c
Britney Wu <Wu.Britney@gmail.com>
2022-04-06 13:18:39 -0400

    added image to education section, still require content

--- src/components/Education.jsx
+++ src/components/Education.jsx
@@ -1,9 +1,18 @@
 import React from 'react'
+import jjcollege from './images/jjcollege.jpg'
 
 function Experience() {
     return(
         <div className='bg-base-300 flex justify-center '>
-            
+            <div class="card lg:card-side bg-base-100 shadow-xl">
+                <figure><img src={johnjaycollege} alt={"johnjaycollege"}/></figure>
+                <div class="card-body">
+                <h2 class="card-title">John Jay College of Criminal Justice</h2>
+                <p>Click the button to listen on Spotiwhy app.</p>
+                <div class="card-actions justify-end">
+                </div>
+                </div>
+            </div>
         </div>
     )
 }export default Experience
