775506b06063f8fc35cd260b1e0d444a9e1914d8
Britney <Wu.Britney@gmail.com>
2022-04-19 12:44:15 -0400

    hover template added

--- src/components/Education.jsx
+++ src/components/Education.jsx
@@ -7,25 +7,25 @@ function Experience() {
         <div className='bg-secondary-content flex justify-center h-screen'>
             <div class='flex-col'>
                 <div class='p-3'>
-                    <div class="card lg:card-side bg-base-100 shadow-xl">
-                        <figure><img src={jjcollege} alt="johnjaycollege"/></figure>
-                            <div class="card-body">
-                                <h2 class="card-title pb-2">John Jay College of Criminal Justice</h2>
+                    <div class='card lg:card-side bg-base-100 shadow-xl'>
+                        <figure><img src={jjcollege} alt='johnjaycollege'/></figure>
+                            <div class='card-body'>
+                                <h2 class='card-title pb-2'>John Jay College of Criminal Justice</h2>
                                     <div class='text-base'>
                                         <p class='pb-2'>B.S in Computer Science</p>
                                         <p class='italic'>Expected graduation: Spring 2023</p>
                                     </div>    
-                                    <div class="card-actions justify-end font-bold">
+                                    <div class='card-actions justify-end font-bold'>
                                         New York, NY
                                     </div>                        
                             </div>
                     </div>
                 </div>
                 <div class='p-3'>
-                <div class="card lg:card-side bg-base-100 shadow-xl">
-                    <figure><img src={ttplogo} alt="ttp-logo"/></figure>
-                        <div class="card-body">
-                            <h2 class="card-title pb-2">Web Development Fellow</h2>
+                <div class='card lg:card-side bg-base-100 shadow-xl'>
+                    <figure><img src={ttplogo} alt='ttp-logo'/></figure>
+                        <div class='card-body'>
+                            <h2 class='card-title pb-2'>Web Development Fellow</h2>
                                 <div class='text-base'>
                                     <p class='italic pb-2'>NYC Tech Talent Pipeline - January 2021</p>
                                         <ul class='list-disc list-inside text-base text-justify'>
@@ -33,7 +33,7 @@ function Experience() {
                                             <li>Completed and contributed to group projects in teams of 8 developers</li>
                                         </ul>
                                 </div>
-                                <div class="card-actions justify-end font-bold">
+                                <div class='card-actions justify-end font-bold'>
                                     New York, NY
                                 </div>
                         </div>
--- src/components/Skills.jsx
+++ src/components/Skills.jsx
@@ -2,16 +2,24 @@ import React from 'react'
 
 function Skills() {
     return(
-        <div className='p-10 text-lg font-bold'>
-            <p>Technologies</p>
-                <ul class='list-disc list-inside text-base text-justify'>
-                    <li>Bullet 1</li>
-                    <li>Bullet 2</li>
-                    <li>Bullet 3</li>
-                </ul>
+        <>
+            <div className='p-10 text-lg font-bold'>
+                <p class='flex justify-center'>Technologies</p>
+                    <div class='card-zoom'>
+                        <div class="container mt-16 mx-auto">
+                            <div class="flex justify-around text-center space-x-16">
+                                <div class="transform h-64 bg-blue-400 w-1/5 hover:bg-blue-600 transition duration-500 hover:scale-125">
+                                box 1
+                                </div>
 
-        </div>
+                            </div>
+    
+                        </div>
+                    </div>
+            </div>
+        </>
     )
+
 }
 
 export default Skills
