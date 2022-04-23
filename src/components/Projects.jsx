c4244d0ce5fbc82008270215b776e5af5d145d1f
Britney Wu <Wu.Britney@gmail.com>
2022-04-23 18:23:44 -0400

    added skills to boxes

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -6,24 +6,24 @@ import { GithubFilled } from '@ant-design/icons'
 function Projects() {
     return(
         <div class='base-300'>
-         <div className='p-12'>
-                <Carousel autoplay effect='fade'>
-                    <div>
-                        <h3 className='text-white text-center text-xl h-80 bg-blue-600'>Github Finder</h3>
-                    </div>
-                    <div>
-                        <h3 className='text-white text-center text-xl h-80 bg-blue-600'>Feedback UI</h3>
-                    </div>
-                    <div>
-                        <h3 className='text-white text-center text-xl h-80 bg-blue-600'>Venue Page</h3>
-                    </div>
-                    <div>
-                        <h3 className='text-white text-center text-xl h-80 bg-blue-600'>Number Guessing Game</h3>
-                    </div>
-                </Carousel>
+            <div className='p-12'>
+                    <Carousel autoplay effect='fade'>
+                        <div>
+                            <h3 className='text-white text-center text-xl h-80 bg-blue-600'>Github Finder</h3>
+                        </div>
+                        <div>
+                            <h3 className='text-white text-center text-xl h-80 bg-blue-600'>Feedback UI</h3>
+                        </div>
+                        <div>
+                            <h3 className='text-white text-center text-xl h-80 bg-blue-600'>Venue Page</h3>
+                        </div>
+                        <div>
+                            <h3 className='text-white text-center text-xl h-80 bg-blue-600'>Number Guessing Game</h3>
+                        </div>
+                    </Carousel>
             </div>
             <div>
-                <div class="card-body ">
+                <div class="card-body">
                     <h2 class="card-title">Github Finder</h2>
                         <p>Description</p>
                             <ul class='list-disc list-inside text-base text-justify'>
@@ -36,31 +36,31 @@ function Projects() {
                             <button class="btn btn-circle btn-primary"><GithubFilled/></button>
                         </div>
                 </div>
-                <div class="card-body ">
+                <div class="card-body">
                     <h2 class="card-title">Feedback UI</h2>
                         <p>Description</p>
-                        <ul class='list-disc list-inside text-base text-justify'>
-                                <li>Bullet 1</li>
-                                <li>Bullet 2</li>
-                                <li>Bullet 3</li>
-                        </ul>
-                    <div class="card-actions justify-end">
-                        <button class="btn btn-circle btn-primary"><ZoomInIcon/></button>
-                        <button class="btn btn-circle btn-primary"><GithubFilled/></button>
-                    </div>
-                </div>
+                            <ul class='list-disc list-inside text-base text-justify'>
+                                    <li>Bullet 1</li>
+                                    <li>Bullet 2</li>
+                                    <li>Bullet 3</li>
+                            </ul>
+                            <div class="card-actions justify-end">
+                                <button class="btn btn-circle btn-primary"><ZoomInIcon/></button>
+                                <button class="btn btn-circle btn-primary"><GithubFilled/></button>
+                            </div>
+                        </div>
                 <div class="card-body ">
                     <h2 class="card-title">Number Guessing Game</h2>
                         <p>Description</p>
-                        <ul class='list-disc list-inside text-base text-justify'>
+                            <ul class='list-disc list-inside text-base text-justify'>
                                 <li>Bullet 1</li>
                                 <li>Bullet 2</li>
                                 <li>Bullet 3</li>
-                        </ul>
-                    <div class="card-actions justify-end">
-                        <button class="btn btn-circle btn-primary"><ZoomInIcon/></button>
-                        <button class="btn btn-circle btn-primary"><GithubFilled/></button>
-                    </div>
+                            </ul>
+                            <div class="card-actions justify-end">
+                                <button class="btn btn-circle btn-primary"><ZoomInIcon/></button>
+                                <button class="btn btn-circle btn-primary"><GithubFilled/></button>
+                            </div>
                 </div>
             </div>
         </div>
--- src/components/Skills.jsx
+++ src/components/Skills.jsx
@@ -3,17 +3,38 @@ import React from 'react'
 function Skills() {
     return(
         <>
-            <div className='p-10 text-lg font-bold'>
-                <p class='flex justify-center'>Technologies</p>
-                    <div class='card-zoom'>
-                        <div class="container mt-16 mx-auto">
-                            <div class="flex justify-around text-center space-x-16">
-                                <div class="transform h-64 bg-blue-400 w-1/5 hover:bg-blue-600 transition duration-500 hover:scale-125">
-                                box 1
+            <div className='p-10 text-xl font-bold'>
+                <p class='text-center'>Skills</p>
+                    <div class='items-stretch'>
+                        <div class='card-zoom'>
+                            <div class="container mt-16">
+                                <div class="flex justify-around text-center space-x-16">
+                                    <div class="transform h-64 bg-blue-400 w-1/5 hover:bg-blue-600 transition duration-500 hover:scale-125">
+                                        <div class='flex flex-col space-y-5 p-5'>
+                                            <p>React</p>
+                                            <p>HTML</p>
+                                            <p>CSS</p>
+                                            <p>Javascript</p>
+                                            <p>C++</p>
+                                        </div>
+                                    </div>
                                 </div>
-
                             </div>
-    
+                        </div>
+                        <div class='card-zoom'>
+                            <div class="container mt-16">
+                                <div class="flex justify-around text-center space-x-16">
+                                    <div class="transform h-64 bg-blue-400 w-1/5 hover:bg-blue-600 transition duration-500 hover:scale-125">
+                                        <div class='flex flex-col space-y-5 p-5'>
+                                            <p>s1</p>
+                                            <p>ds</p>
+                                            <p>fd</p>
+                                            <p>fd</p>
+                                            <p>jh</p>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
                         </div>
                     </div>
             </div>
