29c2220c6433a0106f6fe8de805dec9a33459bf5
Britney Wu <Wu.Britney@gmail.com>
2022-07-01 17:17:56 -0400

    rm background image, add later

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,31 +1,33 @@
 import React from 'react'
 import { FaLinkedin, FaGithub } from 'react-icons/fa'
+import bluehome from '../components/images/bluehome.jpg'
 
 function Home() {  
     return(
         <div class='h-screen bg-cover bg-no-repeat bg-center'>
             <div class="hero-content text-center text-neutral-content">
                 <div class="max-w-md"></div>
-                <h1 class="mb-5 text-5xl font-bold">Hello there</h1>
-                    <p className='text-lg p-12'>
-                    I am currently looking for internships in the field of Web Development or IT. Becoming a Full-Stack Developer is my goal because I enjoy the creative process of planning the layout of websites and using code to make it functional. I look forward to learning frameworks, technologies, and improving my current skillset. In the process of this search, I am pursuing a Bachelors of Science degree in Computer Science at John Jay College.</p>
+                    <h1 class="mb-5 mt-12 text-5xl font-bold">Hello there, I'm Britney Wu</h1>
+                        <p className='text-lg p-12'>
+                        I am currently looking for internships in the field of Web Development or IT. Becoming a Full-Stack Developer is my goal because I enjoy the creative process of planning the layout of websites and using code to make it functional. I look forward to learning frameworks, technologies, and improving my current skillset. In the process of this search, I am pursuing a Bachelors of Science degree in Computer Science at John Jay College.
+                        </p>
             </div>
-            <div className='flex justify-center p-10 gap-x-2'>
-                        <div>
-                                <a href='https://www.linkedin.com/in/britneywu01/' target='_blank' rel='noreferrer'>
-                                    <button class="btn btn-circle btn-xs sm:btn-sm md:btn-md lg:btn-lg">   
-                                        <FaLinkedin />
-                                    </button>
-                                </a>
-                        </div>
-                        <div>
-                                <a href='https://github.com/bw0123' target='_blank' rel='noreferrer'>
-                                    <button class="btn btn-circle btn-xs sm:btn-sm md:btn-md lg:btn-lg">
-                                        <FaGithub />
-                                    </button>
-                                </a>
-                        </div>
+                <div className='flex justify-center p-5 gap-x-2'>
+                    <div>
+                        <a href='https://www.linkedin.com/in/britneywu01/' target='_blank' rel='noreferrer'>
+                            <button class="btn btn-circle btn-xs sm:btn-sm md:btn-md lg:btn-lg">   
+                                <FaLinkedin />
+                            </button>
+                        </a>
                     </div>
+                    <div>
+                        <a href='https://github.com/bw0123' target='_blank' rel='noreferrer'>
+                            <button class="btn btn-circle btn-xs sm:btn-sm md:btn-md lg:btn-lg">
+                                <FaGithub />
+                            </button>
+                        </a>
+                    </div>
+                </div>
         </div>
     )
 }
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -28,11 +28,6 @@ function Navbar() {
     return(
         <div>
             <div class="navbar shadow-lg bg-neutral text-neutral-content text-xl">
-                <div class="flex-none px-2 mx-2">
-                    <span class="font-bold">
-                        Britney Wu
-                    </span>
-                </div> 
                 <div class="flex-1 px-2 mx-2">
                     <div class="items-stretch hidden lg:flex">
                         <Link to='/' class="btn btn-ghost btn-sm rounded-btn">
